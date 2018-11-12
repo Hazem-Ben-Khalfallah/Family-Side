87f9157c4eec6df2714fa58da7adc7777d943f07
Hazem Ben Khalfallah <hazem.benkhalfallah@influans.com>
2018-11-12 18:05:40 +0100

    Fix getting user position

--- app/src/main/java/com/blacknebula/familyside/mobile/activities/MapsActivity.java
+++ app/src/main/java/com/blacknebula/familyside/mobile/activities/MapsActivity.java
@@ -1,12 +1,11 @@
 package com.blacknebula.familyside.mobile.activities;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
@@ -21,6 +20,10 @@ import com.blacknebula.familyside.mobile.util.Logger;
 import com.blacknebula.familyside.mobile.util.ViewUtils;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -31,17 +34,17 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
-    private final int REQUEST_ACCESS_LOCATION_STATE = 1;
-    private final float MIN_ZOOM = 15f;
+    private static final int REQUEST_ACCESS_LOCATION_STATE = 1;
+    private static final int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private static final float MIN_ZOOM = 15f;
     private final String CURRENT_USER_NAME = "You";
 
     private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
     private Marker marker;
+    private LocationRequest mLocationRequest;
 
 
     @Override
@@ -62,20 +65,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .addApi(LocationServices.API)
                     .build();
         }
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000L)        // 10 seconds, in milliseconds
+                .setFastestInterval(1000L); // 1 second, in milliseconds
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onPause() {
+        super.onPause();
         if (mGoogleApiClient != null) {
-            mGoogleApiClient.connect();
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
             mGoogleApiClient.disconnect();
         }
     }
@@ -84,6 +86,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onResume() {
         super.onResume();
         checkGPS();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
     }
 
     private boolean checkGPS() {
@@ -123,38 +128,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setMinZoomPreference(MIN_ZOOM);
-
-        // get my location
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_ACCESS_LOCATION_STATE);
-            return;
-        }
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
     }
 
     @Override
-    public void onConnected(Bundle connectionHint) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLastLocation != null) {
-            addMarkerOnMap(mLastLocation.getLatitude(), mLastLocation.getLongitude(), CURRENT_USER_NAME);
-        } else {
-            ViewUtils.showToast(this, "Move around to help your phone detect you position");
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == REQUEST_ACCESS_LOCATION_STATE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Logger.info(Logger.Type.FAMILY_SIDE, "permission was granted, yay! retry location detection");
+                mGoogleApiClient.connect();
+            } else {
+                Logger.warn(Logger.Type.FAMILY_SIDE, "permission denied, boo! Disable location detection");
+            }
         }
     }
 
-    private void addMarkerOnMap(double lat, double lang, String name) {
-        LatLng position = new LatLng(lat, lang);
-        marker = mMap.addMarker(new MarkerOptions().position(position).title(name));
-        //Move the camera to the user's location and zoom in!
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position, MIN_ZOOM));
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Logger.info(Logger.Type.FAMILY_SIDE, "connection success *********");
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_ACCESS_LOCATION_STATE);
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation == null) {
+            Logger.info(Logger.Type.FAMILY_SIDE, "No last location found. Requesting current location");
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        } else {
+            Logger.info(Logger.Type.FAMILY_SIDE, String.format("Last location detected: Lat: %s Lng: %s", mLastLocation.getLatitude(), mLastLocation.getLongitude()));
+            addMarkerOnMap(mLastLocation.getLatitude(), mLastLocation.getLongitude(), CURRENT_USER_NAME);
+        }
     }
 
     @Override
@@ -164,70 +172,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Logger.error(Logger.Type.FAMILY_SIDE, "Location services connection failed with code %s", connectionResult.getErrorCode());
+        }
     }
 
     @Override
-    public void onLocationChanged(Location loc) {
-        Logger.info(Logger.Type.FAMILY_SIDE, "Location changed: Lat: %s Lng: %s", loc.getLatitude(), loc.getLongitude());
-        moveMarker(marker, new LatLng(loc.getLatitude(), loc.getLongitude()), false);
-    }
+    public void onLocationChanged(final Location loc) {
+        if (marker == null) {
+            ViewUtils.showToast(this, "marker null");
+            addMarkerOnMap(loc.getLatitude(), loc.getLongitude(), CURRENT_USER_NAME);
+        } else {
+            ViewUtils.showToast(this, "marker not null. moving it");
+            final LatLng toPosition = new LatLng(loc.getLatitude(), loc.getLongitude());
+            final Handler handler = new Handler();
+            final long start = SystemClock.uptimeMillis();
+            Projection proj = mMap.getProjection();
+            Point startPoint = proj.toScreenLocation(marker.getPosition());
+            final LatLng startLatLng = proj.fromScreenLocation(startPoint);
+            final long duration = 500; //ms
 
-    /**
-     * move marker around in map
-     *
-     * @param marker     marker
-     * @param toPosition new position
-     * @param hideMarker makes the marker invisible on the map
-     */
-    private void moveMarker(final Marker marker, final LatLng toPosition, final boolean hideMarker) {
-        final Handler handler = new Handler();
-        final long start = SystemClock.uptimeMillis();
-        Projection proj = mMap.getProjection();
-        Point startPoint = proj.toScreenLocation(marker.getPosition());
-        final LatLng startLatLng = proj.fromScreenLocation(startPoint);
-        final long duration = 500; //ms
+            final Interpolator interpolator = new LinearInterpolator();
 
-        final Interpolator interpolator = new LinearInterpolator();
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    long elapsed = SystemClock.uptimeMillis() - start;
+                    float t = interpolator.getInterpolation((float) elapsed
+                            / duration);
+                    double lng = t * loc.getLongitude() + (1 - t)
+                            * startLatLng.longitude;
+                    double lat = t * loc.getLatitude() + (1 - t)
+                            * startLatLng.latitude;
+                    marker.setPosition(new LatLng(lat, lng));
 
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                long elapsed = SystemClock.uptimeMillis() - start;
-                float t = interpolator.getInterpolation((float) elapsed
-                        / duration);
-                double lng = t * toPosition.longitude + (1 - t)
-                        * startLatLng.longitude;
-                double lat = t * toPosition.latitude + (1 - t)
-                        * startLatLng.latitude;
-                marker.setPosition(new LatLng(lat, lng));
-
-                if (t < 1.0) {
-                    // Post again 16ms later.
-                    handler.postDelayed(this, 16);
-                } else {
-                    if (hideMarker) {
-                        marker.setVisible(false);
+                    if (t < 1.0) {
+                        // Post again 16ms later.
+                        handler.postDelayed(this, 16);
                     } else {
                         marker.setVisible(true);
                     }
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(toPosition));
                 }
-                mMap.animateCamera(CameraUpdateFactory.newLatLng(toPosition));
-            }
-        });
+            });
+        }
     }
 
-    @Override
-    public void onProviderDisabled(String provider) {
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
+    private void addMarkerOnMap(double lat, double lang, String name) {
+        LatLng position = new LatLng(lat, lang);
+        marker = mMap.addMarker(new MarkerOptions().position(position).title(name));
+        //Move the camera to the user's location and zoom in!
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position, MIN_ZOOM));
     }
 
 }
